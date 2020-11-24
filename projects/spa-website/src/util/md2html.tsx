9aed5b30d86e396242dfe9f6de1f6a03ae823c9f
LadderOperator <tz_ji@qq.com>
2020-11-24 09:33:06 +0800

    add md css

---
+++ projects/spa-website/src/util/md2html.tsx
@@ -0,0 +1,9 @@
+import ReactMarkdown from "react-markdown";
+import React from "react";
+import "./md.css";
+
+export const ReactMarkdown = (props: any) => {
+    return <ReactMarkdownComponent className="md" {...props} />;
+};
+
+
